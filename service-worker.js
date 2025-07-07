de5f8ec7c2f999196a5fed9e55f5e6a6db7563ec
Ancko <77469180+issitek@users.noreply.github.com>
2025-07-06 19:06:00 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,12 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker instalado');
-    event.waitUntil(
-        caches.open('mi-cache').then(cache => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './app.js'
-            ]);
-        })
-    );
+self.addEventListener("install", (e) => {
+  console.log("Service Worker instalado");
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker activado");
+});
+
+self.addEventListener("fetch", (e) => {
+  // Manejo opcional de peticiones
 });
